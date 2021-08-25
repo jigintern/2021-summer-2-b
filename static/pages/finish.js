0a3e1679c07e80b4b11df0cfd45a82b7d69256ba
thoth000 <snym719a@gmail.com>
2021-08-25 13:26:56 +0900

    fmt

--- static/pages/finish.js
+++ static/pages/finish.js
@@ -1,7 +1,7 @@
 //HomePageに戻る
 export const back_home = () => {
-    document.getElementById("HomePage").style.display = "block";
-    document.getElementById("QuestionPage").style.display = "none";
-    document.getElementById("AnswerPage").style.display = "none";
-    document.getElementById("FinishPage").style.display = "none";
-}
+  document.getElementById("HomePage").style.display = "block";
+  document.getElementById("QuestionPage").style.display = "none";
+  document.getElementById("AnswerPage").style.display = "none";
+  document.getElementById("FinishPage").style.display = "none";
+};
