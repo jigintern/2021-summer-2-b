f8964c0d4b8f67b571f1ea6fe481df4b35be0dba
kouyou-Ki <58977709+kouyou-Ki@users.noreply.github.com>
2021-08-25 13:48:54 +0900

    partial correction

--- static/utils/canvas.js
+++ static/utils/canvas.js
@@ -1,6 +1,7 @@
-import Circle from "./class.js";
+// import Circle from "./class.js";
 
 const Num_of_questions = 5; //問題数
+
 
 // canvasを白く戻す関数
 export const White_canvas = (ctx, x, y) => {
@@ -10,70 +11,94 @@ export const White_canvas = (ctx, x, y) => {
   ctx.restore();
 };
 
-//上の円を表示する関数（オブジェクト別）
-export const UpDrawCircle = (ctx, items) => {
-  [...Array(Num_of_questions)].forEach((_, index) => {
-    const x = 100 + index * 100;
-    const y = 100;
+// 上の円を表示する関数（オブジェクト別）
+// export const UpDrawCircle = (ctx, items) => {
+//   [...Array(Num_of_questions)].forEach((_, index) => {
+//     const x = 100 + index * 100;
+//     const y = 100;
 
-    const circle = new Circle(x, y, 25);
-    items.push(circle);
-  });
+//     const circle = new Circle(x, y, 25);
+//     items.push(circle);
+//   });
 
-  // オブジェクトを描画する関数
-  items.forEach((item) => item.draw(ctx));
-};
+//   // オブジェクトを描画する関数
+//   items.forEach((item) => item.draw(ctx));
+// };
 
 // 下の円を表示する関数
 export const DownDrawCircle = (ctx) => {
   for (let i = 0; i < Num_of_questions; i++) {
-    let xpoint = i * 100;
-    ctx.fillStyle = "black";
-    if (i == 0 || i == Num_of_questions - 1) {
+    let xpoint = i * document.body.clientWidth / 6 ;
+    ctx.fillStyle = "#BBBBBB";
+    if (i === 0 || i === Num_of_questions - 1) {
       ctx.arc(
-        100 + xpoint,
-        450,
-        20,
+        document.body.clientWidth / 6 + xpoint,
+        100,
+        25,
         0 * Math.PI / 180,
         360 * Math.PI / 180,
         false,
       );
     } else {
       ctx.arc(
-        100 + xpoint,
-        450,
-        10,
+        document.body.clientWidth / 6  + xpoint,
+        100,
+        20,
         0 * Math.PI / 180,
         360 * Math.PI / 180,
         false,
       );
     }
     ctx.fill();
-    if (i != Num_of_questions - 1) {
-      DrawHorizontalLine(ctx, 100 + xpoint, 450); //直線の描画
-    }
+    // if (i != Num_of_questions - 1) {
+    //   DrawHorizontalLine(ctx, 100 + xpoint, 100); //直線の描画
+    // }
   }
 };
 
 //下の円の色を変える関数
 export const ChangeCircleColor = (ctx, push_count) => {
-  if (push_count <= Num_of_questions) {
-    ctx.save();
-    ctx.beginPath();
-    ctx.arc(100 * push_count, 450, 25, 0, Math.PI * 2, true);
-    ctx.fillStyle = "blue";
-    ctx.fill();
-    ctx.restore();
+  ctx.save();
+  ctx.beginPath();
+  if (push_count === 0 || push_count === Num_of_questions - 1) {
+    ctx.arc(
+      document.body.clientWidth / 6  * (push_count + 1),
+      100,
+      25,
+      0,
+      Math.PI * 2,
+      true
+    );
+  } else {
+    ctx.arc(
+      document.body.clientWidth / 6  * (push_count + 1),
+      100,
+      20,
+      0,
+      Math.PI * 2,
+      true
+    );
   }
+  ctx.fillStyle = "#3399CC";
+  ctx.fill();
+  ctx.restore();
 };
 
 // 直線をかく関数
-export const DrawHorizontalLine = (ctx, x, y) => {
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = 10;
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(x + 100, y);
-  ctx.closePath();
-  ctx.stroke();
+// export const DrawHorizontalLine = (ctx, x, y) => {
+//   ctx.strokeStyle = "#BBBBBB";
+//   ctx.lineWidth = 10;
+//   ctx.beginPath();
+//   ctx.moveTo(x, y);
+//   ctx.lineTo(x + 100, y);
+//   ctx.closePath();
+//   ctx.stroke();
+// };
+
+// ボタンを押した時の処理
+export const ButtonClick = (ctx, push_count) => {
+  if (push_count < Num_of_questions) {
+    console.log("aaa");
+    ChangeCircleColor(ctx, push_count);
+  }
 };
