1e2bb3bf9dca5f8ba046249757acc43295471a35
kouyou-Ki <58977709+kouyou-Ki@users.noreply.github.com>
2021-08-26 13:52:26 +0900

    問題文と解答追加

--- static/pages/answer.js
+++ static/pages/answer.js
@@ -4,19 +4,28 @@ import { ButtonClick } from "../utils/canvas.js";
 
 export const answerpage = (data,i,ans, score) =>{
     if(ans=="last") ans = data[i].length-1;
-    console.log(i);
-    console.log(data[i].items[ans].name);
-    console.log(data[i].items[ans].score);
+    // console.log(i);
+    // console.log(data[i].items[ans].name);
+    // console.log(data[i].items[ans].score);
+    // console.log(data[i].problem);
     score += data[i].items[ans].score;
 
     let text = document.getElementById("AnswerPage");
 
-    let item_score = document.createElement("h2");
+    let question_text = document.createElement("h3");
+    question_text.textContent = "問題　" + data[i].problem;
+    text.appendChild(question_text);
+
+    let select_tab = document.createElement("p");
+    select_tab.textContent = "解答　" + data[i].items[ans].name;
+    text.appendChild(select_tab);
+
+    let item_score = document.createElement("h1");
     item_score.textContent = data[i].items[ans].score + "点";
     text.appendChild(item_score);
 
     let text_ele = document.createElement("p");
-    text_ele.textContent = data[i].items[ans].description;
+    text_ele.textContent = "解説　" + data[i].items[ans].description;
     text.appendChild(text_ele);
 
     let ok_btn = document.getElementById("AnswerPage");
