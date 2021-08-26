f507b0a78f03bab47ef6a9629003b9912d90e992
thoth000 <snym719a@gmail.com>
2021-08-26 14:09:58 +0900

    push

--- static/pages/home.js
+++ static/pages/home.js
@@ -6,10 +6,17 @@ import { ButtonClick } from "../utils/canvas.js";
 export const homepage = () => {
   let i = 0;
   let score = 0;
-  let testpage = document.getElementById("HomePage");
-  let element = document.createElement("h2");
-  element.textContent = "台風";
-  element.onclick = async function () {
+  let page = document.getElementById("HomePage");
+
+  let modes = document.createElement("div");
+  modes.id = "modes";
+  page.appendChild(modes);
+
+  let div = document.createElement("div");
+  let typhoon = document.createElement("button");
+  typhoon.textContent = "台風";
+  typhoon.className = "mode";
+  typhoon.onclick = async function () {
     const { data } = await fetchJSON("/api/events?subject=typhoon");
     shuffle(data);
     let f = document.getElementById("HomePage");
@@ -21,9 +28,12 @@ export const homepage = () => {
     document.getElementById("canvas").style.display = "block";
     questpage(data, i, score);
   };
-  testpage.appendChild(element);
-  let fire = document.createElement("h2");
+  div.appendChild(typhoon);
+  modes.appendChild(div);
+  div = document.createElement("div");
+  let fire = document.createElement("button");
   fire.textContent = "火災";
+  fire.className = "mode";
   fire.onclick = async function () {
     const { data } = await fetchJSON("/api/events?subject=fire");
     shuffle(data);
@@ -36,6 +46,6 @@ export const homepage = () => {
     document.getElementById("canvas").style.display = "block";
     questpage(data, i, score);
   };
-
-  testpage.appendChild(fire);
+  div.appendChild(fire);
+  modes.appendChild(div);
 };
