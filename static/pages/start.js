1769c31eb5020f24d6a82cbd759a4b7bb9f07ce6
sausukei <68841107+sausukei@users.noreply.github.com>
2021-08-26 11:05:35 +0900

    add fire

--- static/pages/start.js
+++ static/pages/start.js
@@ -1,36 +1,31 @@
-import {shuffle} from "../utils/utils.js";
-import {questpage} from "./question.js";
+import { shuffle } from "../utils/utils.js";
+import { questpage } from "./question.js";
 import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js";
 
 export const firstpage = (i, score) => {
-    let testpage = document.getElementById("QuestionPage");
-    let element = document.createElement("h2");
-    element.textContent = "台風";
-    element.onclick = async function(){
-      const {data} = await fetchJSON("/api/events");
-      shuffle(data);
-      let f = document.getElementById("QuestionPage");
-      while(f.lastChild){
-        f.removeChild(f.lastChild);
-      }
-      const ctx = canvas.getContext("2d");
-      document.getElementById("canvas").style.display = "block";
-      questpage(data,i,score);
+  let testpage = document.getElementById("HomePage");
+  let element = document.createElement("h2");
+  element.textContent = "台風";
+  element.onclick = async function () {
+    const { data } = await fetchJSON("/api/events?subject=typhoon");
+    shuffle(data);
+    let f = document.getElementById("HomePage");
+    while (f.lastChild) {
+      f.removeChild(f.lastChild);
     }
-    testpage.appendChild(element);
-    let fire = document.createElement("h2");
-    fire.textContent = "火災";
-    fire.onclick = async function(){
-      const {data} = await fetchJSON("/api/events");
-      shuffle(data);
-      let f = document.getElementById("QuestionPage");
-      while(f.lastChild){
-        f.removeChild(f.lastChild);
-      }
-      const ctx = canvas.getContext("2d");
-      document.getElementById("canvas").style.display = "block";
-      questpage(data,i,score);
+    questpage(data, i, score);
+  };
+  testpage.appendChild(element);
+  let fire = document.createElement("h2");
+  fire.textContent = "火災";
+  fire.onclick = async function () {
+    const { data } = await fetchJSON("/api/events?subject=fire");
+    shuffle(data);
+    let f = document.getElementById("HomePage");
+    while (f.lastChild) {
+      f.removeChild(f.lastChild);
     }
-    testpage.appendChild(fire);
-    
-  }
+    questpage(data, i, score);
+  };
+  testpage.appendChild(fire);
+};
